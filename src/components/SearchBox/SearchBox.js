b3c72824beb75af703c01e54dd17606c74160ac3
Andrew <78054454+fibonacci2012@users.noreply.github.com>
2023-04-15 17:24:21 +0300

    Adding SearchBox component

---
+++ src/components/SearchBox/SearchBox.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+function SearchBox(props) {
+  const [searchText, setSearchText] = useState('');
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchText}
+        onChange={(event) => setSearchText(event.target.value)}
+      />
+      <button onClick={() => props.onSearch(searchText)}>Search</button>
+    </div>
+  );
+}
+
+export default SearchBox;
