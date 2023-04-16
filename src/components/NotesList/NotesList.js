260ccc891160ec85b2d2487d5b71d4a3509d9f5b
Andrew <78054454+fibonacci2012@users.noreply.github.com>
2023-04-16 21:17:11 +0300

    try to make

--- src/components/NotesList/NotesList.js
+++ src/components/NotesList/NotesList.js
@@ -1,41 +1,57 @@
 import './NotesList.css';
-import React, { useState, useEffect } from 'react';
-import {Button, Card, List} from "antd";
+import React, {useState, useEffect} from 'react';
+import {Button, Card, List, Popconfirm} from "antd";
 import NoteService from "../API/NoteService";
+import imgTrash from "../../rsc/img/trash.png";
 
-const NotesList = () => {
-const [notes, setNotes] = useState([]);
-  async function fetchNotes() {
+const NotesList = ({arrayOfNotes}) => {
+    const [notes, setNotes] = useState([]);
+    const [totalNotes, setTotalNotes] = useState([0]);
+    const imgTrashC = <img src={imgTrash}/>
+    const confirm = () => console.log('Yes')
+    const cancel = () => console.log('No')
+
+    async function fetchNotes() {
         const notes = await NoteService.getAll();
         setNotes(notes.data)
     };
 
     useEffect(() => {
-        fetchNotes()
+        fetchNotes();
     }, [])
 
 
-  return (
-      <List
-    grid={{
-      gutter: 16,
-      xs: 1,
-      sm: 2,
-      md: 4,
-      lg: 4,
-      xl: 6,
-      xxl: 3,
-    }}
-    dataSource={notes}
-    renderItem={(note) => (
-      <List.Item>
-        <Card title={note.title}>{note.body}<Button danger>del</Button></Card>
+    return (
+        <List
+            grid={{
+                gutter: 16,
+                xs: 1,
+                sm: 2,
+                md: 4,
+                lg: 4,
+                xl: 6,
+                xxl: 3,
+            }}
+            dataSource={notes}
+            renderItem={(note) => (
+                <List.Item>
+                    <Card bodyStyle={{height: "120px", width: "120px"}} title={note.title}>{note.body}
+                        <Popconfirm
+                            title="Delete the note"
+                            description="Are you sure to delete this note?"
+                            onConfirm={confirm}
+                            onCancel={cancel}
+                            okText="Yes"
+                            cancelText="No"
+                        >
+                            <Button icon={imgTrashC}></Button>
+                        </Popconfirm></Card>
 
-      </List.Item>
-    )}
-  />
+                </List.Item>
+            )}
+        />
 
-  );
+    );
 };
 
 export default NotesList;
