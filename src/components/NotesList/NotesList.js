97ba18e24477f5783c041909ecf099ec82b70998
Andrew <78054454+fibonacci2012@users.noreply.github.com>
2023-04-17 20:33:06 +0300

    delete button WIP

--- src/components/NotesList/NotesList.js
+++ src/components/NotesList/NotesList.js
@@ -4,17 +4,23 @@ import {Button, Card, List, Popconfirm} from "antd";
 import NoteService from "../API/NoteService";
 import imgTrash from "../../rsc/img/trash.png";
 
-const NotesList = ({arrayOfNotes}) => {
+const NotesList = ({}) => {
     const [notes, setNotes] = useState([]);
     const imgTrashC = <img src={imgTrash}/>
-    const confirm = () => console.log('Yes')
+
+    const confirm = ({remove}) => {
+        const removeNote = (note) => {
+            setNotes(notes.filter(n => n.id !== note.id))
+        };
+    };
+
+
     const cancel = () => console.log('No')
 
     async function fetchNotes() {
         const notes = await NoteService.getAll();
         setNotes(notes.data)
     };
-
     useEffect(() => {
         fetchNotes();
     }, [])
@@ -24,7 +30,7 @@ const NotesList = ({arrayOfNotes}) => {
         <List
             grid={{
                 gutter: 16,
-                xs: 1,
+                xs: 2,
                 sm: 2,
                 md: 4,
                 lg: 4,
@@ -33,18 +39,23 @@ const NotesList = ({arrayOfNotes}) => {
             }}
             dataSource={notes}
             renderItem={(note) => (
-                <List.Item>
-                    <Card bodyStyle={{height: "120px", width: "120px"}} title={note.title}>{note.body}
-                        <Popconfirm
-                            title="Delete the note"
-                            description="Are you sure to delete this note?"
-                            onConfirm={confirm}
-                            onCancel={cancel}
-                            okText="Yes"
-                            cancelText="No"
-                        >
-                            <Button icon={imgTrashC}></Button>
-                        </Popconfirm></Card>
+                <List.Item
+                    //style={{display: "none"}}
+                    extra={<Popconfirm
+                        title="Delete the note"
+                        description="Are you sure to delete this note?"
+                        onConfirm={confirm({})}
+                        onCancel={cancel}
+                        okText="Yes"
+                        cancelText="No"
+                    >
+                        <Button icon={imgTrashC}></Button>
+                    </Popconfirm>}
+                >
+                    <Card bodyStyle={{height: "120px", width: "120px", overflow: "scroll"}}
+                          title={note.title}>{note.body}
+                    </Card>
+
 
                 </List.Item>
             )}
